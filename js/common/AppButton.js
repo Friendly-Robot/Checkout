d93dd80e29abcbd63bc024bda08a4cc84b3ca091
Progoogler <andyzanli@gmail.com>
2019-04-16 23:43:38 -0700

    Commit base button component module

---
+++ js/common/AppButton.js
@@ -0,0 +1,167 @@
+/**
+ * @flow
+ */
+
+"use strict";
+
+import React from "react";
+import AppColors from "./AppColors";
+import { View, StyleSheet, TouchableOpacity, Image, Alert } from "react-native";
+import { Text } from "./AppText";
+
+const BUTTON_HEIGHT = 52,
+  BUTTON_HEIGHT_SM = 32;
+
+class AppButton extends React.Component {
+  props: {
+    theme:
+      | "blue"
+      | "white"
+      | "bordered",
+    type: "default" | "round" | "small",
+    opacity: number,
+    icon?: number,
+    caption?: string,
+    captionStyle?: object,
+    style?: any,
+    fontSize?: number,
+    onPress: () => mixed
+  };
+
+  static defaultProps = {
+    opacity: 1,
+    theme: "white"
+  };
+
+  static height = BUTTON_HEIGHT;
+
+  render() {
+    const { caption, captionStyle, icon, fontSize, opacity } = this.props;
+    const { buttonTheme, iconTheme, captionTheme } = this.getTheme();
+    const { containerType, buttonType, iconType, captionType } = this.getType();
+
+    let iconImage;
+    if (icon) {
+      iconImage = (
+        <Image source={icon} style={[styles.icon, iconTheme, iconType]} />
+      );
+    }
+
+    let fontSizeOverride;
+    if (fontSize) {
+      fontSizeOverride = { fontSize };
+    }
+
+    const content = (
+      <View style={[styles.button, buttonTheme, buttonType, { opacity }]}>
+        <Text
+          style={[styles.caption, captionTheme, captionType, fontSizeOverride, captionStyle]}
+        >
+          {caption}
+        </Text>
+        {iconImage}
+      </View>
+    );
+
+    if (this.props.onPress) {
+      return (
+        <TouchableOpacity
+          accessibilityTraits="button"
+          onPress={this.props.onPress}
+          activeOpacity={0.5}
+          style={[styles.container, containerType, this.props.style]}
+        >
+          {content}
+        </TouchableOpacity>
+      );
+    } else {
+      return (
+        <View style={[styles.container, containerType, this.props.style]}>
+          {content}
+        </View>
+      );
+    }
+  }
+
+  getTheme() {
+    const { theme } = this.props;
+    let buttonTheme, iconTheme, captionTheme;
+    if (theme === "bordered") {
+      buttonTheme = {
+        backgroundColor: "transparent",
+        borderColor: AppColors.primary,
+        borderWidth: 1
+      };
+      iconTheme = { tintColor: AppColors.primary };
+      captionTheme = { color: AppColors.primary };
+    } else if (theme === "blue") {
+      buttonTheme = { backgroundColor: AppColors.blue };
+      iconTheme = { tintColor: AppColors.white };
+      captionTheme = { color: AppColors.white };
+    } else if (theme === "white") {
+      buttonTheme = { backgroundColor: AppColors.white };
+      iconTheme = { tintColor: AppColors.primary };
+      captionTheme = { color: AppColors.primary };
+    } else if (theme === "none") {
+      buttonTheme = { backgroundColor: "transparent", padding: 0 };
+      iconTheme = { tintColor: AppColors.primary };
+      captionTheme = { color: AppColors.primary };
+    } else {
+      buttonTheme = { backgroundColor: AppColors.primary };
+      iconTheme = { tintColor: AppColors.white };
+      captionTheme = { color: AppColors.white };
+    }
+
+    return { buttonTheme, iconTheme, captionTheme };
+  }
+
+  getType() {
+    const { type } = this.props;
+    let containerType, buttonType, iconType, captionType;
+
+    if (type === "round") {
+      buttonType = { width: BUTTON_HEIGHT, paddingHorizontal: 0 };
+      iconType = { marginRight: 0 };
+      captionType = { fontSize: 13 };
+    } else if (type === "small") {
+      containerType = { height: BUTTON_HEIGHT_SM };
+      buttonType = { paddingHorizontal: 15 };
+      iconType = { marginRight: 0 };
+      captionType = { fontSize: 13 };
+    } else if (type === "none") {
+      captionType = { textAlign: "left" };
+      buttonType = { justifyContent: "flex-start", paddingHorizontal: 0, borderRadius: 0 };
+    } else {
+      // defaults
+    }
+
+    return { containerType, buttonType, iconType, captionType };
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: BUTTON_HEIGHT
+  },
+  button: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 30,
+    borderRadius: BUTTON_HEIGHT / 2
+  },
+  buttonRound: {
+    width: BUTTON_HEIGHT,
+    paddingHorizontal: 0
+  },
+  icon: {
+    marginRight: 12
+  },
+  caption: {
+    fontSize: 15,
+    textAlign: "center"
+  }
+});
+
+export default AppButton;
