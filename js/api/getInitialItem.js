d1c66fa86b59536b55f0c3d86dffe63863c29135
Progoogler <andyzanli@gmail.com>
2019-04-16 23:40:21 -0700

    Update naming convention

--- js/api/getInitialItem.js
+++ js/api/getInitialItem.js
@@ -8,7 +8,7 @@ const getInitialItem: () => object = () => ({
     price: 102.96,
     savings: 15.00,
     tax: 6.00,
-    zip: 94086,
+    zipcode: 94086,
     quantity: 1,
     name: "Essentials by OFM ESS-3085 Racing Style Leather Gaming Chair, Red",
     img: "https://i5.walmartimages.com/asr/e73e1252-642c-4473-93ea-fd3b564a7027_1.3e81ea58fa3042452fe185129a4a865f.jpeg?odnWidth=undefined&odnHeight=undefined&odnBg=ffffff",
