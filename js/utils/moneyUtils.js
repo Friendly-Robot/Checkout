87a9e4ecbe5e5a23f9fc9ff125afe54c73637ea7
Progoogler <andyzanli@gmail.com>
2019-04-17 14:30:47 -0700

    Add parseFloat operation to fixed() to transform default string type to number

--- js/utils/moneyUtils.js
+++ js/utils/moneyUtils.js
@@ -4,12 +4,12 @@
 
 "use strict";
 
-const fixed = num => (num / 100).toFixed(2);
+const fixed = num => typeof num === "number" ? parseFloat((num / 100).toFixed(2)) : num;
 
-const x100 = num => num * 100;
+const x100 = num => typeof num === "number" ? num * 100 : num;
 
 function sum() {
-  return fixed(Array.prototype.reduce.call(arguments, (acc, curr) => acc += x100(curr), 0))
+  return fixed(Array.prototype.reduce.call(arguments, (acc, curr) => acc += x100(curr), 0));
 }
 
 export {
