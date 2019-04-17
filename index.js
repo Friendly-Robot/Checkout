d076281eb5a692996ddd0606835b7e24d0c28d9e
Progoogler <andyzanli@gmail.com>
2019-04-16 18:23:29 -0700

    Call setup() to return Root component

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ import {AppRegistry} from 'react-native';
 import setup from './js/setup';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => setup);
+AppRegistry.registerComponent(appName, () => setup());
