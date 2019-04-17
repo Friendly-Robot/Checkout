5b567a771d6d5a34f40a1a0d6913f25c1ae4ac9e
Progoogler <andyzanli@gmail.com>
2019-04-17 14:13:02 -0700

    Add condition to return immediately if item passed into second argument does not contain name property

--- js/utils/itemUtils.js
+++ js/utils/itemUtils.js
@@ -7,6 +7,7 @@
 import type { Item } from "../actions/summary/types";
 
 const findIndexOfItem: (array: array, item: Item) => number = (array, item) => {
+  if (!item || !item.name) return NaN;
   for (let i = 0; i < array.length; i += 1) {
     if (array[i].name === item.name) {
       return i;
